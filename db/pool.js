4109072951342bf0b9f913f79894ae6e4d24b17f
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-05-12 21:48:16 -0400

    Reconfigure Pool DB File for Deployment

--- db/pool.js
+++ db/pool.js
@@ -5,9 +5,5 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
+    connectionString: process.env.DATABASE_URL,
 });
