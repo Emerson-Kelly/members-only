c2bd5e2a95bc8962fedf18b6ae38c91144a7bc0d
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-05-12 21:54:48 -0400

    Remove SSL in Pool DB File

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,6 @@ dotenv.config();
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false, // Needed for Railway
-    },
-  });
+});
